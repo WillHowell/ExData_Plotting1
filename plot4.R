eca91c63e7df6be5cde2e25dba658fe79d1ea83b
WillHowell <will.howell@gmail.com>
2014-05-11 17:56:30 -0400

    removed comment markers on lines--meant to sped up execution since data was already loaded

--- plot4.R
+++ plot4.R
@@ -2,11 +2,11 @@
 library("sqldf")
 
 #if the table exists from a previous run, delete it
-#sqldf("drop table if exists main.elec", dbname="testingdb")
+sqldf("drop table if exists main.elec", dbname="testingdb")
 
 #read the raw text file into main.elec
-#read.csv.sql("Data//household_power_consumption.txt", sql = "create table main.elec as select * from file",
-#             dbname = "testingdb",sep=";")
+read.csv.sql("Data//household_power_consumption.txt", sql = "create table main.elec as select * from file",
+             dbname = "testingdb",sep=";")
 
 #use sql to filter to the two days we're interessted in and load that into "filtered"
 filtered <- sqldf("select * from main.elec where Date = '1/2/2007' or Date = '2/2/2007'", dbname = "testingdb")
